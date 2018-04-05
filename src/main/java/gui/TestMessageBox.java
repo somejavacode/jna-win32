14ada4f7f89fcaa3142789df71f3db0ce3eea6d1
Some Code <somecodegithub@gmail.com>
2018-04-05 17:33:04 +0200

    add box sample

---
+++ src/main/java/gui/TestMessageBox.java
@@ -0,0 +1,24 @@
+package gui;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.win32.W32APIOptions;
+
+public class TestMessageBox {
+
+    private final static int MB_YES_NO = 0x4;
+    private final static int MB_ICON_EXCLAMATION = 0x30; // yellow triangle
+
+    // this is a "stub" that will be mapped (as it is not already mapped in JNA)
+    public interface user32 extends Library {
+        int MessageBox(int something, String text, String caption, int flags);
+    }
+
+    public static void main(String[] args) throws Exception {
+        user32 lib = Native.loadLibrary("user32", user32.class, W32APIOptions.UNICODE_OPTIONS);
+
+        // blocking
+        int win = lib.MessageBox(0, "MessageBox success ÖÄÖÄ!!!",  "Attention", MB_YES_NO | MB_ICON_EXCLAMATION);
+        System.out.println("got window=" + win);  // 7 NO, 6 YES ?
+    }
+}
